7269acc79320f564645b17772d4a45dfa31ec6a1
Avinash Tiwari <avikvvv@gmail.com>
2024-03-19 23:00:04 +0530

    add: backend to upload state files to third party storages

--- aws/terraform.tf
+++ aws/terraform.tf
@@ -1,3 +1,11 @@
 terraform {
   required_version = "1.7.4"
+
+  # remote backend file, to store the remote file in remote location
+  # backend "s3" {
+  #   bucket         = "test-tf-states-1"
+  #   region         = "us-east-2"
+  #   key            = "terraform.tfstate"
+  #   dynamodb_table = "table-name" # table to avoid race-around condition
+  # }
 }
