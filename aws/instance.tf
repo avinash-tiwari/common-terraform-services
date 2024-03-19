fd15ff04a29a6a5a83ba97b77411f1cfad35af7b
Avinash Tiwari <avikvvv@gmail.com>
2024-03-19 19:23:34 +0530

    add: add support for provisioners

--- aws/instance.tf
+++ aws/instance.tf
@@ -8,4 +8,24 @@ resource "aws_instance" "web" {
     Name = "CreatedFromTerraform"
   }
   user_data = file("${path.module}/init.sh")
+
+  # connection settings to be used by provisioners
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("${path.module}/id_rsa")
+    host        = self.public_ip
+  }
+
+  # file, local-exec, remote-exec
+  # in case of error, it will mark the instance as taint
+  provisioner "file" {
+    source      = "id_rsa.pub"
+    destination = "/tmp/id_rsa.pub"
+
+  }
+  provisioner "file" {
+    content     = "Written from terraform"
+    destination = "/tmp/readme.md"
+  }
 }
